ccd988050e750173c3cfa70e3aa660ed9aabfda8
keenan <keenanphillips@gmail.com>
2016-09-25 11:31:46 -0700

    crawl works but difficulty identifying images in crawled pages

--- content.js
+++ content.js
@@ -1,34 +1,64 @@
 function imageFind() {
   let imgs = document.getElementsByTagName("img");
-  let imgSrcs = [];
-  // debugger
-  for (let i = 0; i < imgs.length; i++) {
-    if (imgs[i].height > 25 && imgs[i].width > 25){
-      imgSrcs.push(imgs[i].src);
+  [].slice.call(imgs).forEach((img) => {
+    if(img.parentNode.nodeName === "A"){
+      chrome.runtime.sendMessage({text: "crawl", content: {href: img.parentNode.href, url: img.src}})
+    } else {
+      chrome.runtime.sendMessage({text: "get-tags", content: img.src})
     }
-  }
+    // crawlForBestImageUrl(imgElement)
+  })
+  // let imgSrcs = [];
+  // debugger
+  // for (let i = 0; i < imgs.length; i++) {
+  //   if (imgs[i].height > 25 && imgs[i].width > 25){
+  //     imgSrcs.push(imgs[i].src);
+  //   }
+    // [].slice.call(imgs).forEach((img) => {
+    //
+    //   crawlForBestImageUrl(img)
+    // })
+  // }
 
-  return imgSrcs;
+  // return imgSrcs;
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+  console.log("getting to content");
   if(request.text === "grab-images"){
-    chrome.runtime.sendMessage({text: "images", content: imageFind()})
+    imageFind()
   }
 })
 
-console.log("content.js is running");
+// console.log("content.js is running");
 
 
 // function crawlForBestImageUrl(imgElement){
+//   let imageUrl = imgElement.src
 //   if(imgElement.parentNode.nodeName === "A"){
-//     let xhr = new XMLHttpRequest
-//     xhr.open("GET", imgElement.parentNode.href);
-//     xhr.responseType = "document";
-//     xhr.send()
-//     xhr.response
+//     // let xhr = new XMLHttpRequest
+//     // xhr.open("GET", imgElement.parentNode.href);
+//     // xhr.responseType = "document";
+//     // xhr.onload = function (e) {
+//     //   if (xhr.readyState === 4) {
+//     //     if (xhr.status === 200) {
+//     //       console.log(xhr.responseText);
+//     //     } else {
+//     //       console.error(xhr.statusText);
+//     //     }
+//     //   }
+//     // };
+//     // xhr.send()
+//     $.ajax({
+//       'url': imgElement.parentNode.href,
+//       'type': 'GET',
+//       success: function (response) {
+//           console.log(response);
+//       }
+//     });
+//     debugger
 //   } else {
-//     return imgElement.src
+//     chrome.runtime.sendMessage({text: "image", content: imageUrl})
 //   }
 // }
 // aTagsWithNestedImages = [].slice.call(document.getElementsByTagName("a")).filter((el) => (el.getElementsByTagName("img").length === 1))
--- popup.js
+++ popup.js
@@ -39,22 +39,93 @@ function downloadIfMatched(responseObject){
 function getTags(url) {
   console.log("ajax for image at" + url);
   $.ajax({
-          'url': 'https://api.clarifai.com/v1/tag',
-          'headers': {
-              'Authorization': 'Bearer ' + 'HHLwiLlvQ79YVFj4aAqTWPIXbMibcY'
-          },
-          'data': {url: url},
-          'type': 'POST',
-          success: function (response) {
-              downloadIfMatched(response);
-          }
-      });
+    'url': 'https://api.clarifai.com/v1/tag',
+    'headers': {
+        'Authorization': 'Bearer ' + 'HHLwiLlvQ79YVFj4aAqTWPIXbMibcY'
+    },
+    'data': {url: url},
+    'type': 'POST',
+    success: function (response) {
+        downloadIfMatched(response);
+    }
+  });
 };
 
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+//   if (request.text === "image"){
+//     // request.content.forEach((image) => {
+//       getTags(request.content)
+//     // })
+//   }
+// })
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-  if (request.text === "images"){
-    request.content.forEach((image) => {
-      getTags(image)
-    })
+  if (request.text === "crawl"){
+    // [].slice.call(request.content).forEach((imageLinkData) => {
+      crawlForBestImageUrl(request.content)
+    // })
+  }
+  if (request.text === "get-tags"){
+    getTags(request.content)
   }
 })
+
+function crawlForBestImageUrl(imageLinkData){
+  console.log("crawling");
+  console.log(imageLinkData.href);
+  let originalUrl = imageLinkData.url
+  // let imageUrl = imageLinkData.src
+  // if(imageLinkData.href === "A"){
+    // let xhr = new XMLHttpRequest
+    // xhr.open("GET", imageLinkData.parentNode.href);
+    // xhr.responseType = "document";
+    // xhr.onload = function (e) {
+    //   if (xhr.readyState === 4) {
+    //     if (xhr.status === 200) {
+    //       console.log(xhr.responseText);
+    //     } else {
+    //       console.error(xhr.statusText);
+    //     }
+    //   }
+    // };
+    // xhr.send()
+    $.ajax({
+      'url': imageLinkData.href,
+      'type': 'GET',
+      success: function (response) {
+        locateMainImageAndCallAPI(response, originalUrl)
+          // debugger// here we can add another helper function that parses the returned data from the link and tries to find a bigger image link. if it doesnt find one, we call getTags on the original url
+      }
+    });
+    // debugger
+  // } else {
+    // getTags(imageUrl)
+  // }
+}
+
+function locateMainImageAndCallAPI(response, url){
+  let imgs = []
+  let imgElements = $(response).find("img")
+  for (i = 0; i < imgElements.length; i++){
+    if (imgElements[i] && imgElements[i].width > 50 && imgElements[i].height > 50){
+      imgs.push(imgElements[i])
+    }
+  }
+  if (imgs.length === 1){
+    let src = imgs[0].src
+    if(src[0] === "/")
+    getTags(imgs[0].src)//this is a single large image on a page, getTags(this image)
+  } else {
+    getTags(url)//we didnt find a link leading to a full sized image, just use the original url
+  }
+}
+
+
+// let imgs = $(response).find("img").filter((img) => (//this filter is not working for some reason... cant figure out why?
+//   img.width > 50 && img.height > 50
+// ))
+// let imgs
+// let imgElements = $(response).find("img")
+// for (i = 0; i < imgElements.length; i++){
+//   if imgElements[i].width > 50 && imgElements[i].height > 50
+//   imgs.push(imgElements[i])
+// }
